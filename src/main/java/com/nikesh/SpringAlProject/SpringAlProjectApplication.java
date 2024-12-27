0deab39faa7a870699b9cf9b009cc0e9c28852fd
rajputnikesh <yournewemail@example.com>
2024-12-27 17:09:15 +0530

    Initial commit

---
+++ src/main/java/com/nikesh/SpringAlProject/SpringAlProjectApplication.java
@@ -0,0 +1,13 @@
+package com.nikesh.SpringAlProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAlProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAlProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nikesh/SpringAlProject/SpringAlProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikesh.SpringAlProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAlProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
